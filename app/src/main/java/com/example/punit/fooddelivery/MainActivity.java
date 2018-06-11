1816360e6b9557a03ace15b78db23c582cff0393
Pj18 <punit18071999@gmail.com>
2018-06-11 22:56:30 +0530

    Added random items to main page

--- app/src/main/java/com/example/punit/fooddelivery/MainActivity.java
+++ app/src/main/java/com/example/punit/fooddelivery/MainActivity.java
@@ -26,11 +26,13 @@ import android.widget.Toast;
 
 import com.example.punit.fooddelivery.FoodContract.FoodEntry;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     CartHelper cDBHelper=new CartHelper(this);
-
+    //DBHelper mDBHelper=new DBHelper(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,11 +62,111 @@ public class MainActivity extends AppCompatActivity
         cDBHelper.deletedb(db);
 
 
-
+        info();
 
     }
 
+    private void info()
+    {
 
+        SQLiteDatabase db = mDBHelper.getReadableDatabase();
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.homepage1);
+        String projection[] = {
+                FoodContract.FoodEntry.COLUMN_NAME,
+                FoodContract.FoodEntry.COLUMN_PRICE,
+                FoodContract.FoodEntry.COLUMN_IMG
+        };
+
+        Cursor cursor = db.query(
+                FoodContract.FoodEntry.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                "Random()",
+                "3"
+
+        );
+
+        try
+
+        {
+
+            while (cursor.moveToNext()) {
+                LayoutInflater layoutInflater = (LayoutInflater) getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                final View addView = layoutInflater.inflate(R.layout.eatables, null);
+                String NameF = cursor.getString(cursor.getColumnIndex(FoodContract.FoodEntry.COLUMN_NAME));
+                String PriceF = cursor.getString(cursor.getColumnIndex(FoodContract.FoodEntry.COLUMN_PRICE));
+                String IMGF = cursor.getString(cursor.getColumnIndex(FoodContract.FoodEntry.COLUMN_IMG));
+                final TextView t1 = (TextView) addView.findViewById(R.id.foodname);
+                final TextView t2 = (TextView) addView.findViewById(R.id.foodprice);
+                ImageView i1 = (ImageView) addView.findViewById(R.id.foodimage);
+                Button addb=(Button)addView.findViewById(R.id.addbutton);
+                Button decb=(Button)addView.findViewById(R.id.decreasebutton);
+                Button addcart=(Button)addView.findViewById(R.id.addtocartbutton);
+                final TextView itemquant=(TextView)addView.findViewById(R.id.quantitybutton);
+
+
+                addb.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        int num=Integer.parseInt(itemquant.getText().toString());
+
+                        num++;
+                        itemquant.setText(String.valueOf(num));
+                    }
+                });
+                decb.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        int num=Integer.parseInt(itemquant.getText().toString());
+                        if(num>0)
+                        {
+                            num--;
+                            itemquant.setText(String.valueOf(num));
+                        }
+                    }
+                });
+                addcart.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(Integer.parseInt(itemquant.getText().toString())>0){
+                            SQLiteDatabase db=cDBHelper.getWritableDatabase();
+                            //Toast.makeText(MainActivity.this,itemquant.getText().toString(),Toast.LENGTH_LONG).show();
+                            ContentValues values=new ContentValues();
+                            values.put(CartContract.CartEntry.CART_NAME,t1.getText().toString());
+                            values.put(CartContract.CartEntry.CART_PRICE,Integer.parseInt(t2.getText().toString()));
+                            values.put(CartContract.CartEntry.CART_QUANTITY,Integer.parseInt(itemquant.getText().toString()));
+                            int id=(int)db.update(CartContract.CartEntry.TABLE_NAME,values, CartContract.CartEntry.CART_NAME+"=?",new String[] {t1.getText().toString()});
+                            if(id==0)
+                            {
+                                db.insertWithOnConflict(CartContract.CartEntry.TABLE_NAME,null,values,SQLiteDatabase.CONFLICT_IGNORE);
+
+                            }
+                        }
+                    }
+                });
+                t1.setText(NameF);
+                t2.setText(PriceF);
+                i1.setImageResource(getResources().getIdentifier(""+IMGF, "drawable", getPackageName()));
+                layout.addView(addView);
+
+                LinearLayout addView1=new LinearLayout(this);
+                addView1.setLayoutParams(new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,25
+                ));
+                layout.addView(addView1);
+            }
+        }finally
+
+        {
+            cursor.close();
+        }
+    }
 
     @Override
     public void onBackPressed() {
